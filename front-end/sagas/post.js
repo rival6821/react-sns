160e56eef24b370883ca55a633e9fcbc7aa1b352
ilhoon <rival6821@naver.com>
2019-05-23 21:20:30 +0900

    사가 반복문 제어

--- front-end/sagas/post.js
+++ front-end/sagas/post.js
@@ -1,5 +1,5 @@
 import { all } from "redux-saga/effects";
 
-export default function* postSage() {
+export default function* postSaga() {
   yield all([]);
 }
--- front-end/sagas/user.js
+++ front-end/sagas/user.js
@@ -6,7 +6,9 @@ import {
   HELLO
 } from "../reducers/user";
 
-function* loginAPI() {}
+function* loginAPI() {
+  // 서버에 요청을 보내는 부분
+}
 
 function* login() {
   try {
@@ -22,11 +24,20 @@ function* login() {
   }
 }
 
+// LOG_IN액션이 실행되면 LOG_IN_SUCCESS액션이 자동으로 실행된다.
+// 한번만 하고 사라짐 계속 반복되게 하려면 while문 적용해야함
 function* watchLogin() {
-  yield takeLatest(LOG_IN, login);
+  while (true) {
+    yield take(LOG_IN);
+    // put은 dispatch랑 동일
+
+    yield put({
+      type: LOG_IN_SUCCESS
+    });
+  }
 }
 
-function* helloSage() {
+function* helloSaga() {
   console.log("before");
   while (true) {
     yield take(HELLO);
@@ -34,7 +45,6 @@ function* helloSage() {
   }
 }
 
-export default function* userSage() {
-  //yield all([fork(watchLogin)]);
-  yield helloSage();
+export default function* userSaga() {
+  yield all([fork(watchLogin)]);
 }
